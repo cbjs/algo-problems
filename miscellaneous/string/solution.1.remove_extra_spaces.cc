ca08ca5a402c7bc81ece41cb005637f0f6da443e
cbjs <cbjs.deng@gmail.com>
2013-09-12 11:29:08 +0800

    modify miscellaneous/string/1

--- miscellaneous/string/solution.1.remove_extra_spaces.cc
+++ miscellaneous/string/solution.1.remove_extra_spaces.cc
@@ -3,11 +3,13 @@
 using namespace std;
 
 char* remove_extra_spaces(char* str) {
+  if (!str || !*str) return str;
+
   char* i = str;
   char* j = str;
 
   // strip spaces at the begining
-  while (*j && *j == ' ') ++j;
+  while (*j == ' ') ++j;
 
   // remove spaces in the middle
   while(*j) {
@@ -23,10 +25,33 @@ char* remove_extra_spaces(char* str) {
   return str;
 }
 
+char* remove_extra_spaces_arr(char* str) {
+  if (!str || !*str) return str;
+
+  int i = -1;
+  int j = 0;
+  while (str[j] == ' ') ++j;
+
+  while(str[j]) {
+    if (str[j] != ' ' || str[i] != ' ') {
+      str[++i] = str[j];
+    }
+    j++;
+  }
+
+  if (i != -1 && str[i] == ' ') str[i] = '\0';
+  else str[i + 1] = '\0';
+  return str;
+}
+
 int main(int argc, const char *argv[])
 {
   char str[] = "  abc def   aga ada   xxx    ";
   std::cout << "before:" << str << "$" << std::endl;
   std::cout << "after:" << remove_extra_spaces(str) << "$" << std::endl;
+
+  char str1[] = "  abc def   aga ada   xxx    ";
+  std::cout << "before:" << str1 << "$" << std::endl;
+  std::cout << "after:" << remove_extra_spaces_arr(str1) << "$" << std::endl;
   return 0;
 }
