71549678a5da5b4be8ba4c256226cebe3dcee485
cbjs <cbjs.deng@gmail.com>
2013-09-12 14:22:57 +0800

    add comment for miscellaneous/array/4

--- miscellaneous/array/solution.4.kth_number_in_two_sorted_array.cc
+++ miscellaneous/array/solution.4.kth_number_in_two_sorted_array.cc
@@ -2,13 +2,25 @@
 
 using namespace std;
 
+// 假设数组c是a和b合并排序后的数组
+// 比较中位数，数组下标从0开始, a有la, b有lb个元素
+//  若a[ma] >= b[mb], 在数组c中，b[mb]之前(包括b[mb])的mb + 1个数一定出现在a[ma]之前
+//                      则a[ma]之前至少有ma + mb + 1个数
+//                    在数组c中, a[ma]之后(包括a[ma])的la - ma个元素一定出现在b[mb]之后
+//                      则b[mb]之后至少有(la + lb) - (ma + mb + 1)个数
+//    若k <= ma + mb + 1, 则只需在a[0, ma)和b[0, end)中找第K大的数
+//    若k > ma + mb + 1, 则在a[0, end]和b[mb + 1, end)中找第K - mb - 1大的数
+//  若a[ma] < b[mb]同理分析
 int kth_num(int arra[], int as, int ae, int arrb[], int bs, int be, int k) {
   // 如果其中一个数组为空，返回另外一个数组中第k大的数
   if (bs >= be) return arra[as + k - 1];
   if (as >= ae) return arrb[bs + k - 1];
 
+  // m,n为两个数组大小
   int m = ae - as;
   int n = be - bs;
+
+  // 比较中位数
   if (arra[as + m/2] >= arrb[bs + n/2]) {
     if (k <= (m/2 + n/2 + 1)) {
       return kth_num(arra, as, as + m/2, arrb, bs, be, k);
