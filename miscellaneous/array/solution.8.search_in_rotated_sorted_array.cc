6bb224e52743be4294ecbde9e7fb01b0576933e3
cbjs <cbjs.deng@gmail.com>
2013-10-23 13:19:36 +0800

    add m/array/8 m/string/7

---
+++ miscellaneous/array/solution.8.search_in_rotated_sorted_array.cc
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool search(int A[], int n, int key) {
+    int l = 0, r = n - 1;
+    while (l <= r) {
+        int m = l + (r - l)/2;
+        if (A[m] == key) return true; //return m in Search in Rotated Array I
+        if (A[l] < A[m]) { //left half is sorted
+            if (A[l] <= key && key < A[m])
+                r = m - 1;
+            else
+                l = m + 1;
+        } else if (A[l] > A[m]) { //right half is sorted
+            if (A[m] < key && key <= A[r])
+                l = m + 1;
+            else
+                r = m - 1;
+        } else l++;
+    }
+    return false;
+}
+};
