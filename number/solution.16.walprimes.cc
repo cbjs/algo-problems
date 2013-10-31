c862aae3992ad841ed7283fb43b7f9d9c80f135c
cbjs <cbjs.deng@gmail.com>
2013-10-31 21:08:02 +0800

    m number/solution.16

--- number/solution.16.walprimes.cc
+++ number/solution.16.walprimes.cc
@@ -3,28 +3,34 @@
 
 using namespace std;
 
-int walprimes(const string& s) {
-  return _walprimes(s, 2) + _walprimes(s, 3) + _walprimes(s, 5) + _walprimes(s, 7)
-        - _walprimes(s, 2 * 3) - _walprimes(s, 2 * 5) - _walprimes(s, 2 * 7)
-        - _walprimes(s, 3 * 5) - _walprimes(s, 3 * 7) - _walprimes(s, 5 * 7)
-        + _walprimes(s, 2 * 3 * 5) + _walprimes(s, 3 * 5 * 7)
-        - _walprimes(s, 2 * 3 * 5 * 7);
-}
-
-int _walprimes(const string& s, int p) {
+int F(const string& s, int p) {
   int n = s.size(), remain[n][p];
   memset(remain, 0, sizeof(remain));
 
   for (int i = n - 1; i >= 0; i--) {
     int first = s[i] - '0';
+
     for (int k = i + 1; k < n; k++) {
       for (int j = 0; j < p; j++) {
-        remain[i]
+        // -1 % 2 = -1 replaced with (2 + -1 % 2) % 2 = 1
+        remain[i][j] += remain[k][(p + (j - first) % p) % p];
+        remain[i][j] += remain[k][(p + (first - j) % p) % p];
       }
+
       first = 10 * first + s[k] - '0';
     }
+
     remain[i][first % p] += 1;
   }
+
+  return remain[0][0];
+}
+
+int walprimes(const string& s) {
+  return F(s, 2) + F(s, 3) + F(s, 5) + F(s, 7)
+       - F(s, 2 * 3) - F(s, 2 * 5) - F(s, 2 * 7) - F(s, 3 * 5) - F(s, 3 * 7) - F(s, 5 * 7)
+       + F(s, 3 * 5 * 7) + F(s, 2 * 5 * 7) + F(s, 2 * 3 * 7) + F(s, 2 * 3 * 5)
+       - F(s, 2 * 3 * 5 * 7);
 }
 
 int main(int argc, const char *argv[])
