654ab52ef63b84d6f96ab5a95232ef0275bd9863
cbjs <cbjs.deng@gmail.com>
2013-11-01 13:04:14 +0800

    m number/15

--- number/solution.15.single_number_ii.cc
+++ number/solution.15.single_number_ii.cc
@@ -1,5 +1,17 @@
 class Solution {
 public:
+    int singleNumberDirect(int A[], int n) {
+      int result = 0, m = sizeof(int) * 8;
+      for (int i = 0; i < m; i++) {
+        int cur = 0;
+        for (int j = 0; j < n; j++) {
+          cur += (A[j] >> i) & 0x01;
+        }
+        result |= (cur % 3) << i;
+      }
+      return result;
+    }
+
     int singleNumber(int A[], int n) {
         int one = 0, two = 0;
         for (int i = 0; i < n; i++) {
