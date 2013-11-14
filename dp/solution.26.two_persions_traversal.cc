befd3f6b5d6db8e5b1030d82485d960e9fb1bcfe
cbjs <cbjs.deng@gmail.com>
2013-11-15 00:19:31 +0800

    m dp/s.26

--- dp/solution.26.two_persions_traversal.cc
+++ dp/solution.26.two_persions_traversal.cc
@@ -10,46 +10,32 @@ vector<vector<int> > min_distance(const vector<vector<int> >& d) {
   int n = d.size();
   vector<vector<int> > partition(2, vector<int>());
 
-  int S[n], T[n], D[n], A[n];
-  memset(S, 0, sizeof(S));
-  memset(T, 0, sizeof(T));
+  int D[n + 1][n + 1], T[n + 1];
   memset(D, 0, sizeof(D));
-  memset(A, 0, sizeof(A));
+  memset(T, 0, sizeof(T));
 
-  for (int i = 0; i < n; i++) {
-    int minj = i - 1;
-    S[i] = INT_MAX;
-    for (int j = i - 1; j >= 0; j--) {
-      int curdis = S[j] + D[j + 1] + d[j][i];
-      int k = j - 1;
-      while (k >= 0 && T[k] == T[j]) k--;
-      if (k >= 0) curdis += d[k][j + 1];
-      if (S[i] > curdis) {
-        S[i] = curdis;
-        minj = j;
+  for (int i = 1; i <= n; i++) {
+    // D[i][j], j < i - 1
+    for (int j = 0; j < i - 1; j++) {
+      D[i][j] = D[i - 1][j] + d[i - 2][i - 1];
+    }
+    // D[i][i - 1]
+    D[i][i - 1] = INT_MAX;
+    for (int j = 0; j < i - 1; j++) {
+      int curdis = D[i - 1][j] + (j > 0 ? d[j - 1][i - 1] : 0);
+      if (D[i][i - 1] > curdis) {
+        D[i][i - 1] = curdis;
+        T[i] = j;
       }
-      if (i >= 2 && j < i - 1) D[j] += d[i - 2][i - 1];
-    }
-    if (S[i] > D[0]) {
-      S[i] = D[0];
-      minj = -1;
-    }
-
-    int k = 0;
-    if (minj == -1) {
-      T[i] = 1;
-    } else {
-      T[i] = T[minj];
-      k = 1 - T[i];
-    }
-    for (int j = minj + 1; j < i; j++) {
-      T[j] = k;
     }
   }
 
-  cout << "min_distance:" << S[n - 1] << endl;
+  int k = 0;
+  for (int i = 1; i < n; i++) {
+    if (D[n][i] < D[n][k]) i = k;
+  }
+  cout << "min_distance:" << D[n][k] << endl;
 
-  for (int i = 0; i < n; i++) partition[T[i]].push_back(i);
   return partition;
 }
 
