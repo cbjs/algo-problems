aba90b18e8ad93cd4a5c52507ade721c7d5ec627
cbjs <cbjs.deng@gmail.com>
2013-11-23 10:22:17 +0800

    modify dp/s.27

--- dp/solution.27.function_approximation.cc
+++ dp/solution.27.function_approximation.cc
@@ -1,12 +1,16 @@
-#include <climits.h>
+#include <climits>
+#include <math.h>
 #include <string.h>
+#include <iostream>
+
+using namespace std;
 
 struct Point {
   double x;
   double y;
 };
 
-double error_between(Point ps[], int i, int j) {
+double e(Point ps[], int i, int j) {
   if (j - i <= 1) return 0.0;
   double grad = (ps[j].y - ps[i].y) / (ps[j].x - ps[i].x), errors = 0.0;
   for (int k = i + 1; k < j; k++) {
@@ -16,18 +20,44 @@ double error_between(Point ps[], int i, int j) {
   return errors;
 }
 
-double min_errors(Point ps[], int n, int k) {
-  if (n <= 2 || n <= k) return 0.0;
-  if (k < 2) return
+double min_errors(Point ps[], int N, int K) {
+  if (N <= 2 || N <= K) return 0.0;
+  if (K < 2) return INFINITY;
+  if (K == 2) return e(ps, 0, N - 1);
 
-  double error[n][k];
+  double error[N][K];
   memset(error, 0, sizeof(error));
 
-  for (int i = n - 3; i >= 0; i--) {
-    for (int j = i + 1; j < n; j++) {
-      for (int i = 0; i < min(k, ); i++) {
-        /* code */
+  for (int i = 2; i < N; i++) {
+    error[i][1] = e(ps, 0, i);
+    for (int k = 2; k < K; k++) error[i][k] = error[i][1];
+
+    for (int j = 0; j < i; j++) {
+      double et = e(ps, j, i);
+      for (int k = 2; k < K; k++) {
+        double ec = et + error[j][k - 1];
+        if (ec < error[i][k]) error[i][k] = ec;
       }
     }
   }
+
+  return error[N - 1][K - 1];
+}
+
+int main(int argc, char *argv[])
+{
+  Point ps[] = {
+    {0, 0},
+    {0.5, 0.23},
+    {1, 0.5},
+    {2, 2.3},
+    {3, 1.8},
+    {4, 4.1}
+  };
+  int N = sizeof(ps) / sizeof(Point);
+  int K = N - 2;
+  cout << N << endl;
+  cout << K << endl;
+  cout << "min_errors:" << min_errors(ps, N, K) << endl;
+  return 0;
 }
