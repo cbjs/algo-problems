8a37798545f716473b0b510ec7cd366d82048c81
cbjs <cbjs.deng@gmail.com>
2013-12-01 19:42:42 +0800

    add m/array/p.15

--- dp/solution.30.max_absolute_difference.cc
+++ dp/solution.30.max_absolute_difference.cc
@@ -15,26 +15,30 @@ int maxAbsoluteDiff(int arr[], int n) {
     return arr[0];
   }
 
-  int lmin[n], lmax[n], rmin[n], rmax[n];
+  int lmin[n], lmax[n], lmini[n], lmaxi[n];
 
   memcpy(lmin, arr, n * sizeof(int));
   memcpy(lmax, arr, n * sizeof(int));
-  memcpy(rmin, arr, n * sizeof(int));
-  memcpy(rmax, arr, n * sizeof(int));
+  memcpy(lmini, 0, n * sizeof(int));
+  memcpy(lmaxi, 0, n * sizeof(int));
 
   for (int i = 1; i < n; i++) {
+    lmini[i] = lmaxi[i] = i;
     if (lmin[i - 1] < 0) {
       lmin[i] += lmin[i - 1];
+      lmini[i] = lmini[i - 1];
     }
     if (lmax[i - 1] > 0) {
       lmax[i] += lmax[i - 1];
+      lmaxi[i] = lmaxi[i - 1];
     }
   }
 
+  int rmin = arr[n - 1], rmax = arr[n - 1], rmini = n - 1, rmaxi = n - 1;
   for (int i = n - 2; i >= 0; i--) {
-    if (rmin[i + 1] < 0)
+    if (rmin < 0)
     {
-      rmin[i] += rmin[i + 1];
+      rmin += arr[i];
     }
     if (rmax[i + 1] > 0)
     {
